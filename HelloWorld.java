0d465a6952a90b0bb1bce7a6d98ba60fa6225ca6
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2023-12-15 21:31:19 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld
 {
-    public static void main(String args[]){
-        System.out.println("Hello World");
+    public static void main(String args[])
+    {
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i + ". Hello Hao, Huy");
+        }
     }
 }
