d46a83a73b1070c0469024943917bdf53a5e279e
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2023-12-16 21:21:01 +0100

    update version moi

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,10 @@
 public class HelloWorld
 {
+    public static void sayHello() {
+        System.out.println("Hello world");
+    }
     public static void main(String args[])
     {
-        for(int i = 0; i < 5; i++) {
-            System.out.println(i + ". Hello Guys");
-        }
-        int a = 2;
-        System.out.println("Huy");
-        System.out.println("1+1 =" + a);
+        sayHello();
     }
 }
