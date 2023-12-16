065debbb84b52e484db46f8901859aabfca70e1a
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2023-12-16 21:58:09 +0100

    version 1

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+    public static void sayHello()
+    {
+        System.out.println("Hello team");
+    }
+
+    public static void main(String[] args)
+    {
+        sayHello();
+    }
+}
