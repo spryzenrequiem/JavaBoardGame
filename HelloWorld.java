9924a0d2a553faead5df5c3c0b28c731441db2c9
vovuongbaohuy <10421021@student.vgu.edu.vn>
2023-12-16 22:13:50 +0100

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,10 @@
 public class HelloWorld
 {
-    public static void sayHello()
-    {
-        System.out.println("Hello team");
-    }
 
-    public static void hello()
-    {
-        System.out.println("version 2");
-    }
 
-    public static void main(String[] args)
-    {
-        sayHello();
+    public static void main(String[] args){
+
         System.out.println("Check nha");
-
-        hello();
     }
 
 }
