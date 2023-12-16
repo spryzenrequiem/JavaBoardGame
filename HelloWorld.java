f49d289f74018c7d1cbb7b8c23c00fb695d62dea
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2023-12-16 22:08:24 +0100

    version 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,17 @@ public class HelloWorld
         System.out.println("Hello team");
     }
 
+    public static void hello()
+    {
+        System.out.println("version 2");
+    }
+
     public static void main(String[] args)
     {
         sayHello();
         System.out.println("Check nha");
+
+        hello();
     }
 
 }
