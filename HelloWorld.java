7f3e30e06fc5fde3c525a4001bd8a81f2a50393c
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2023-12-16 21:34:17 +0100

    Update HelloWorld.java
    Delete file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,1 @@
-public class HelloWorld
-{
-    public static void sayHello() {
-        System.out.println("Hello world");
-    }
-    public static void main(String args[])
-    {
-        sayHello();
-    }
-}
+
