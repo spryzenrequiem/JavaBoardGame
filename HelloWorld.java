9ae788f24fe512fc3bbb657d1b579618dce1c021
JTrinh03 <107232361+JTrinh03@users.noreply.github.com>
2023-12-17 03:13:48 +0700

    tao update them cai nay

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld
         for(int i = 0; i < 5; i++) {
             System.out.println(i + ". Hello Guys");
         }
-
+        int a = 2;
         System.out.println("Huy");
+        System.out.println("1+1 =" + a);
     }
 }
